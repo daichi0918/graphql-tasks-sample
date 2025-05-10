d103e5caad20a79946d40ad8dae149a7b190ab15
daichi0918 <kayasugadaichi@icloud.com>
2025-05-10 17:21:18 +0900

    fin:chapter15

---
+++ backend/src/task/task.resolver.ts
@@ -0,0 +1,22 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { TaskService } from './task.service';
+import { Task } from './models/task.model';
+
+@Resolver()
+export class TaskResolver {
+  constructor(private readonly taskService: TaskService) {}
+
+  // @Query(() => [Task], { nullable: 'items' })
+  // getTasks(): Task[] {
+  //   return this.taskService.getTasks();
+  // }
+  @Query(() => [Task], { nullable: 'items' })
+  getTasks(): Task[] {
+    return this.taskService.getTasks();
+  }
+
+  // @Mutation(() => Task)
+  // createTask(@Args('createTaskInput') createTaskInput: CreateTaskInput): Task {
+  //   return this.taskService.createTask(createTaskInput);
+  // }
+}
