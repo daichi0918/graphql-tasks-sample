4a284a2a8bb7d2002f7c152aa8f2c2bcac291ae1
daichi0918 <kayasugadaichi@icloud.com>
2025-05-16 17:33:07 +0900

    finish:chapter45

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,25 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SignIn from './components/Signin';
+import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import Main from './components/Main';
 import NotFound from './components/NotFount';
+import { GuestRoute, PrivateRoute } from './AuthRoute';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/" element={<Main />} />
+          <Route
+            path="/signin"
+            element={<GuestRoute children={<SignIn />} />}
+          />
+          <Route
+            path="/signup"
+            element={<GuestRoute children={<SignUp />} />}
+          />
+          <Route path="/" element={<PrivateRoute children={<Main />} />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
