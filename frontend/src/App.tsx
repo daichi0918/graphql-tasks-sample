0f18a0834339202c4050e1cf2e75a543cd958e08
daichi0918 <kayasugadaichi@icloud.com>
2025-05-16 17:23:07 +0900

    finish:chapter44

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './components/Signin';
+import SignUp from './components/SignUp';
+import Main from './components/Main';
+import NotFound from './components/NotFount';
 
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Main />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
